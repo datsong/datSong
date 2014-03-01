a1c32a51fa2347bc7575d364bdf6d6506db316f5
ryanmagno <ryan.magno@gmail.com>
2014-03-01 15:49:46 -0500

    ryan's inital COMMIT OK

---
+++ cloud/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+	$studentEmail="d@datsong.com";
+	
+	$databaseName="datSong";
+	$databaseHostName="localhost";
+	$databaseUser="almightyducks1";
+	$databasePassword = "codEd1120";
+	$usersTable="betaUsers";
+
+	//social connect
+	$facebookAppId="1375786532641902";
+	$twitterAppId=null;
+
+
+
+
+
+
+?>
